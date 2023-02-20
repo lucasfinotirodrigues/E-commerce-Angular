451591d4408b38e060fe7a2b9730c80c5dcf0a25
lucas_finoti <lucasfinotirodrigues@hotmail.com>
2023-02-19 22:42:11 -0300

    feat: Criação do NgFor do card do componente action-figures

--- src/app/pages/action-figures/action-figures.component.ts
+++ src/app/pages/action-figures/action-figures.component.ts
@@ -7,6 +7,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActionFiguresComponent implements OnInit {
 
+
+  //Lista de personagens para preencher pelos cards
+
+
+  lista = [
+    
+    {src:'../../../assets/af03.jpg', nome: 'Monkey D. Luffy',tamanho:'21', preco:'189,90' },
+
+    {src:'../../../assets/af01.jpg', nome: 'Roronoa Zoro',tamanho:'18', preco:'200,90' },    
+    
+    {src:'../../../assets/af09.jpg', nome: 'Nami',tamanho:'18', preco:'289,90' },
+    
+    {src:'../../../assets/af20.jpg', nome: 'Usopp',tamanho:'22', preco:'395,90' },    
+    
+    {src:'../../../assets/af15.jpg', nome: 'Vinsmoke Sanji',tamanho:'18', preco:'200,90' },    
+    
+    {src:'../../../assets/af16.jpg', nome: 'Chopper',tamanho:'15', preco:'100,90' },    
+    
+    {src:'../../../assets/af05.jpg', nome: 'Nico Robin',tamanho:'17', preco:'208,90' },  
+
+    {src:'../../../assets/af18.jpg', nome: 'Franky',tamanho:'26', preco:'173,90' },
+    
+    {src:'../../../assets/af17.jpg', nome: 'Brook',tamanho:'27', preco:'150,90' },
+    
+    {src:'../../../assets/af19.jpg', nome: 'Jimbei',tamanho:'21', preco:'246,90' },    
+
+    {src:'../../../assets/af02.jpg', nome: 'Shanks o ruivo',tamanho:'22', preco:'225,90' },    
+
+    {src:'../../../assets/af04.jpg', nome: 'Ace e Luffy',tamanho:'24', preco:'285,90' },
+    
+    {src:'../../../assets/af06.jpg', nome: 'Gol D. Roger',tamanho:'23', preco:'317,90' },    
+    
+    {src:'../../../assets/af07.jpg', nome: 'Yamato',tamanho:'21', preco:'305,90' },
+    
+    {src:'../../../assets/af08.jpg', nome: 'Portgas D. Ace',tamanho:'20', preco:'246,90' },
+    
+    {src:'../../../assets/af10.jpg', nome: 'Barba Branca',tamanho:'23', preco:'309,90' },
+    
+    {src:'../../../assets/af11.jpg', nome: 'Marco a Fenix',tamanho:'22', preco:'299,90' },
+    
+    {src:'../../../assets/af12.jpg', nome: 'Charlotte Katakuri',tamanho:'20', preco:'199,90' },
+    
+    {src:'../../../assets/af13.jpg', nome: 'Doflamingo',tamanho:'25', preco:'199,90' },
+    
+    {src:'../../../assets/af14.jpg', nome: 'Trafalgar D. Law',tamanho:'26', preco:'217,90' },
+    
+    {src:'../../../assets/af27.jpg', nome: 'Kaido',tamanho:'24', preco:'317,90' },
+    
+    {src:'../../../assets/af28.jpg', nome: 'Raleigh',tamanho:'22', preco:'308,90' },
+    
+    {src:'../../../assets/af24.jpg', nome: 'Boa Hancock',tamanho:'18', preco:'197,90' },
+    
+    {src:'../../../assets/af21.jpg', nome: 'Bartolomeo',tamanho:'19', preco:'163,90' },
+    
+    {src:'../../../assets/af22.jpg', nome: 'Enel',tamanho:'20', preco:'180,90' },
+    
+    {src:'../../../assets/af23.jpg', nome: 'Mihawk',tamanho:'22', preco:'300,90' },
+    
+    {src:'../../../assets/af25.jpg', nome: 'Crocodile',tamanho:'24', preco:'187,90' },
+    
+    {src:'../../../assets/af26.jpg', nome: 'Big Mom',tamanho:'21', preco:'301,90' },
+
+  
+    
+  
+  ];
   constructor() { }
 
   ngOnInit(): void {
