88dee8f6402ff62f2e4c2515c9cf1e5e60a65394
lucas_finoti <lucasfinotirodrigues@hotmail.com>
2023-02-20 17:58:31 -0300

    style: Estilizando os novos componentes dos action-figures

--- src/app/pages/action-figures/action-figures.component.ts
+++ src/app/pages/action-figures/action-figures.component.ts
@@ -36,59 +36,7 @@ export class ActionFiguresComponent implements OnInit {
     {src:'../../../assets/af02.jpg', nome: 'Shanks o ruivo',tamanho:'22', preco:'225,90' },    
 
     {src:'../../../assets/af04.jpg', nome: 'Ace e Luffy',tamanho:'24', preco:'285,90' },
-    
-    {src:'../../../assets/af06.jpg', nome: 'Gol D. Roger',tamanho:'23', preco:'317,90' },    
-    
-    {src:'../../../assets/af07.jpg', nome: 'Yamato',tamanho:'21', preco:'305,90' },
-    
-    {src:'../../../assets/af08.jpg', nome: 'Portgas D. Ace',tamanho:'20', preco:'246,90' },
-    
-    {src:'../../../assets/af10.jpg', nome: 'Barba Branca',tamanho:'23', preco:'309,90' },
-    
-    {src:'../../../assets/af11.jpg', nome: 'Marco a Fenix',tamanho:'22', preco:'299,90' },
-    
-    {src:'../../../assets/af12.jpg', nome: 'Charlotte Katakuri',tamanho:'20', preco:'199,90' },
-    
-    {src:'../../../assets/af13.jpg', nome: 'Doflamingo',tamanho:'25', preco:'199,90' },
-    
-    {src:'../../../assets/af14.jpg', nome: 'Trafalgar D. Law',tamanho:'26', preco:'217,90' },
-    
-    {src:'../../../assets/af27.jpg', nome: 'Kaido',tamanho:'24', preco:'317,90' },
-    
-    {src:'../../../assets/af28.jpg', nome: 'Raleigh',tamanho:'22', preco:'308,90' },
-    
-    {src:'../../../assets/af24.jpg', nome: 'Boa Hancock',tamanho:'18', preco:'197,90' },
-    
-    {src:'../../../assets/af21.jpg', nome: 'Bartolomeo',tamanho:'19', preco:'163,90' },
-    
-    {src:'../../../assets/af22.jpg', nome: 'Enel',tamanho:'20', preco:'180,90' },
-    
-    {src:'../../../assets/af23.jpg', nome: 'Mihawk',tamanho:'22', preco:'300,90' },
-    
-    {src:'../../../assets/af25.jpg', nome: 'Crocodile',tamanho:'24', preco:'187,90' },
-    
-    {src:'../../../assets/af26.jpg', nome: 'Big Mom',tamanho:'21', preco:'301,90' },
 
-    
-    {src:'../../../assets/af29.jpg', nome: 'Barba Negra',tamanho:'24', preco:'191,90' },
-    
-    {src:'../../../assets/af30.jpg', nome: 'Kuma',tamanho:'23', preco:'249,90' },
-    
-    {src:'../../../assets/af32.jpg', nome: 'Eustass Kid',tamanho:'25', preco:'343,90' },
-    
-    {src:'../../../assets/af31.jpg', nome: 'Gekko Moria',tamanho:'20', preco:'211,90' },
-
-    
-    {src:'../../../assets/af34.jpg', nome: 'Rob Lucci',tamanho:'20', preco:'256,90' },
-    
-    {src:'../../../assets/af33.jpg', nome: 'Oden',tamanho:'20', preco:'118,90' },
-    
-    {src:'../../../assets/af35.jpg', nome: 'Buggy',tamanho:'28', preco:'123,90' },
-    
-    {src:'../../../assets/af36.jpg', nome: 'Sabo',tamanho:'26', preco:'308,90' },
-
-  
-    
   
   ];
   constructor() { }
--- src/app/pages/action-figures2/action-figures2.component.ts
+++ src/app/pages/action-figures2/action-figures2.component.ts
@@ -7,6 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActionFigures2Component implements OnInit {
 
+
+  lista = [
+  
+
+    
+    {src:'../../../assets/af06.jpg', nome: 'Gol D. Roger',tamanho:'23', preco:'317,90' },    
+    
+    {src:'../../../assets/af07.jpg', nome: 'Yamato',tamanho:'21', preco:'305,90' },
+    
+    {src:'../../../assets/af08.jpg', nome: 'Portgas D. Ace',tamanho:'20', preco:'246,90' },
+    
+    {src:'../../../assets/af10.jpg', nome: 'Barba Branca',tamanho:'23', preco:'309,90' },
+    
+    {src:'../../../assets/af11.jpg', nome: 'Marco a Fenix',tamanho:'22', preco:'299,90' },
+    
+    {src:'../../../assets/af12.jpg', nome: 'Charlotte Katakuri',tamanho:'20', preco:'199,90' },
+    
+    {src:'../../../assets/af13.jpg', nome: 'Doflamingo',tamanho:'25', preco:'199,90' },
+    
+    {src:'../../../assets/af14.jpg', nome: 'Trafalgar D. Law',tamanho:'26', preco:'217,90' },
+    
+    {src:'../../../assets/af27.jpg', nome: 'Kaido',tamanho:'24', preco:'317,90' },
+    
+    {src:'../../../assets/af28.jpg', nome: 'Raleigh',tamanho:'22', preco:'308,90' },
+    
+    {src:'../../../assets/af24.jpg', nome: 'Boa Hancock',tamanho:'18', preco:'197,90' },
+    
+    {src:'../../../assets/af21.jpg', nome: 'Bartolomeo',tamanho:'19', preco:'163,90' },
+
+  ]
   constructor() { }
 
   ngOnInit(): void {
