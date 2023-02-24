d303759471b9bd6f5fc9c27a747505e9126481e2
lucas_finoti <lucasfinotirodrigues@hotmail.com>
2023-02-24 10:01:43 -0300

    feat: Criando o componente roupas-femininas

---
+++ src/app/pages/roupas-femininas/roupas-femininas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-roupas-femininas',
+  templateUrl: './roupas-femininas.component.html',
+  styleUrls: ['./roupas-femininas.component.scss']
+})
+export class RoupasFemininasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
