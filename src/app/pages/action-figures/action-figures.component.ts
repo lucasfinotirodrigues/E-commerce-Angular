b7efba61acbf67545975734ca4683e159d51d8ab
lucas_finoti <lucasfinotirodrigues@hotmail.com>
2023-03-17 21:15:08 -0300

    feat: Criando alteração do ícone de curtir dos 3 componentes action-figures

--- src/app/pages/action-figures/action-figures.component.ts
+++ src/app/pages/action-figures/action-figures.component.ts
@@ -13,33 +13,43 @@ export class ActionFiguresComponent implements OnInit {
 
   lista = [
     
-    {src:'../../../assets/af03.jpg', nome: 'Monkey D. Luffy',tamanho:'21', preco:'189,90' },
+    {src:'../../../assets/af03.jpg', nome: 'Monkey D. Luffy',tamanho:'21', preco:'189,90', default:'../../../assets/heart.svg',},
 
-    {src:'../../../assets/af01.jpg', nome: 'Roronoa Zoro',tamanho:'18', preco:'200,90' },    
+    {src:'../../../assets/af01.jpg', nome: 'Roronoa Zoro',tamanho:'18', preco:'200,90', default:'../../../assets/heart.svg'}, 
     
-    {src:'../../../assets/af09.jpg', nome: 'Nami',tamanho:'18', preco:'289,90' },
+    {src:'../../../assets/af09.jpg', nome: 'Nami',tamanho:'18', preco:'289,90',        default:'../../../assets/heart.svg'},
     
-    {src:'../../../assets/af20.jpg', nome: 'Usopp',tamanho:'22', preco:'395,90' },    
+    {src:'../../../assets/af20.jpg', nome: 'Usopp',tamanho:'22', preco:'395,90' ,          default:'../../../assets/heart.svg'},   
     
-    {src:'../../../assets/af15.jpg', nome: 'Vinsmoke Sanji',tamanho:'18', preco:'200,90' },    
+    {src:'../../../assets/af15.jpg', nome: 'Vinsmoke Sanji',tamanho:'18', preco:'200,90',          default:'../../../assets/heart.svg'},    
     
-    {src:'../../../assets/af16.jpg', nome: 'Chopper',tamanho:'15', preco:'100,90' },    
+    {src:'../../../assets/af16.jpg', nome: 'Chopper',tamanho:'15', preco:'100,90',          default:'../../../assets/heart.svg'},    
     
-    {src:'../../../assets/af05.jpg', nome: 'Nico Robin',tamanho:'17', preco:'208,90' },  
+    {src:'../../../assets/af05.jpg', nome: 'Nico Robin',tamanho:'17', preco:'208,90',          default:'../../../assets/heart.svg'},  
 
-    {src:'../../../assets/af18.jpg', nome: 'Franky',tamanho:'26', preco:'173,90' },
+    {src:'../../../assets/af18.jpg', nome: 'Franky',tamanho:'26', preco:'173,90',          default:'../../../assets/heart.svg'},
     
-    {src:'../../../assets/af17.jpg', nome: 'Brook',tamanho:'27', preco:'150,90' },
+    {src:'../../../assets/af17.jpg', nome: 'Brook',tamanho:'27', preco:'150,90',          default:'../../../assets/heart.svg'},
     
-    {src:'../../../assets/af19.jpg', nome: 'Jimbei',tamanho:'21', preco:'246,90' },    
+    {src:'../../../assets/af19.jpg', nome: 'Jimbei',tamanho:'21', preco:'246,90',          default:'../../../assets/heart.svg'},   
 
-    {src:'../../../assets/af02.jpg', nome: 'Shanks o ruivo',tamanho:'22', preco:'225,90' },    
+    {src:'../../../assets/af02.jpg', nome: 'Shanks o ruivo',tamanho:'22', preco:'225,90',          default:'../../../assets/heart.svg'},    
 
-    {src:'../../../assets/af04.jpg', nome: 'Ace e Luffy',tamanho:'24', preco:'285,90' },
+    {src:'../../../assets/af04.jpg', nome: 'Ace e Luffy',tamanho:'24', preco:'285,90',          default:'../../../assets/heart.svg'},
 
   
   ];
-  constructor() { }
+
+  alterIcon(index: number) {
+    // Altere apenas um item do card usando a indexação do array
+    this.lista[index].default = '../../../assets/heart.png';
+  }
+
+  constructor() {
+  
+   }
+
+   alteracaoIcone = true;
 
   ngOnInit(): void {
   }
